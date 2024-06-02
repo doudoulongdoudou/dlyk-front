eb2cc8a35f9fb68b3b0be4695907f658eab3adda
ddldd <feng0039@gmail.com>
2024-06-02 21:59:17 +0800

    动力云客-前端-基础工程

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue()],
+
+    server: {
+        //ip地址
+        host: '0.0.0.0',
+        //设置服务启动端口号
+        port: 8081,
+        //设置服务启动时是否自动打开浏览器
+        open: false,
+    }
+
+})
